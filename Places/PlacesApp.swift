4bb42ad5124998b9f017e3f7674f99510964963b
Amirhossein Validabadi <amirhossein.validabadi@gmail.com>
2025-04-03 10:49:26 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Amirhossein Validabadi on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
